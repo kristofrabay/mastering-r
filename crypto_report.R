38c9057910afd71a7094acd8e7daa3dcd14b1cb4
kristofrabay <kristof.rabay@gmail.com>
2020-04-15 12:41:38 +0000

    adding log msgs

--- crypto_report.R
+++ crypto_report.R
@@ -7,3 +7,20 @@ print(paste0('Value of .42 Bitcoins is HUF ', round(0.42 * binance_coins_prices(
 
 # other solutions
 fromJSON(readLines('https://api.exchangeratesapi.io/latest?base=USD'))
+
+
+# making script more efficient
+library(logger)
+
+BITCOINS <- 0.42
+log_info('Number of Bitcoins: {BITCOINS}') 
+
+## TODO the Binance API is a bit of a mess .. need to add retries
+btcusdt <- binance_coins_prices()[symbol == 'BTC']$usd
+log_info('Value of 1 BTC in USDT is: {btcusdt}') 
+
+usdhuf <- fromJSON("https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF")$rates$HUF
+log_info('Value of 1 USD in HUF is: {usdhuf}')
+
+
+BITCOINS * btcusdt * usdhuf
