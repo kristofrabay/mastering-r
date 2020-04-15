2867ab271c86126d7587dd8737f83313f72e62c1
kristofrabay <kristof.rabay@gmail.com>
2020-04-15 14:04:57 +0000

    formatting

--- crypto_report.R
+++ crypto_report.R
@@ -1,19 +1,10 @@
 library(binancer)
 library(jsonlite)
 library(logger)
-library(checkmate) # adding validations
+library(checkmate)
+library(scales)
 log_threshold(TRACE)
 
-print(paste0('Value of .42 Bitcoins is $', round(0.42 * binance_coins_prices()[symbol == 'BTC']$usd, 2)))
-
-# in HUF: fromJSON("https://api.exchangeratesapi.io/latest?symbols=USD,HUF")$rates$HUF api call
-print(paste0('Value of .42 Bitcoins is HUF ', round(0.42 * binance_coins_prices()[symbol == 'BTC']$usd * (fromJSON("https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF")$rates$HUF), 0)))
-
-# other solutions
-fromJSON(readLines('https://api.exchangeratesapi.io/latest?base=USD'))
-
-
-# making script more efficient
 
 BITCOINS <- 0.42
 log_info('Number of Bitcoins: {BITCOINS}') 
@@ -29,20 +20,25 @@ log_info('Number of Bitcoins: {BITCOINS}')
 # }
 
 
-get_bitcoin_price <- function() {
+get_bitcoin_price <- function(retried = 0) {
   tryCatch(binance_coins_prices()[symbol == 'BTC']$usd,
-           error = function (e) {get_bitcoin_price()}) # function to call itself
+           error = function (e) {
+             # exponential backoff retries
+             Sys.sleep(1 + retried ^ 2)
+             get_bitcoin_price(retried = retried + 1)
+           })
 }
 
 
 btcusdt <- get_bitcoin_price()
-log_info('Value of 1 BTC in USDT is: {btcusdt}') 
-# log_eval(btcusdt) # evaluate expression and log results
-assert_number(btcusdt, lower = 1000) # lets assert if it is a number
+log_info('Value of 1 BTC in USDT is: {dollar(btcusdt)}') 
+assert_number(btcusdt, lower = 1000) 
+
+forint <- function(x) {dollar(x, suffix = "Ft", prefix = NULL)}
 
 usdhuf <- fromJSON("https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF")$rates$HUF
-log_info('Value of 1 USD in HUF is: {usdhuf}')
+log_info('Value of 1 USD in HUF is: {forint(usdhuf)}')
 assert_number(usdhuf, lower = 250, upper = 500)
 
 
-log_eval(BITCOINS * btcusdt * usdhuf) # TODO formatting
+log_eval(forint(BITCOINS * btcusdt * usdhuf))
