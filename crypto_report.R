a60d8352929d704c08c5a63ad25af31b47f19790
kristofrabay <kristof.rabay@gmail.com>
2020-04-15 12:57:04 +0000

    adding checkmate

--- crypto_report.R
+++ crypto_report.R
@@ -26,19 +26,21 @@ log_info('Number of Bitcoins: {BITCOINS}')
 # }
 
 
-# function to call itself
-
 get_bitcoin_price <- function() {
   tryCatch(binance_coins_prices()[symbol == 'BTC']$usd,
-           error = function (e) {get_bitcoin_price()})
+           error = function (e) {get_bitcoin_price()}) # function to call itself
 }
 
 
 btcusdt <- get_bitcoin_price()
 log_info('Value of 1 BTC in USDT is: {btcusdt}') 
+# lets assert if it is a number
+library(checkmate) # adding validations
+assert_number(btcusdt, lower = 1000)
 
 usdhuf <- fromJSON("https://api.exchangeratesapi.io/latest?base=USD&symbols=HUF")$rates$HUF
 log_info('Value of 1 USD in HUF is: {usdhuf}')
+assert_number(usdhuf, lower = 250, upper = 500)
 
 
 BITCOINS * btcusdt * usdhuf
